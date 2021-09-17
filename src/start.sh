f15c1ef32bbea01a9851022e4d1b71e22b924cd2
Aloïs GAUCHER <agaucher@eutech.fr>
2021-09-17 09:37:48 +0200

    fix(agent): fix agent not sending data

--- src/start.sh
+++ src/start.sh
@@ -9,19 +9,19 @@ cleanup() {
     exit
 }
 
-cp etc/hetrixtools_dl/hetrixtools_install_cp.sh etc/hetrixtools_dl/hetrixtools_install.sh
+cp /etc/hetrixtools_dl/hetrixtools_install_cp.sh /etc/hetrixtools_dl/hetrixtools_install.sh
 echo "***Copied hetrixtools_install_cp.sh to hetrixtools_install.sh"
 
-chmod --reference=etc/hetrixtools_dl/hetrixtools_install_cp.sh etc/hetrixtools_dl/hetrixtools_install.sh
+chmod --reference=/etc/hetrixtools_dl/hetrixtools_install_cp.sh /etc/hetrixtools_dl/hetrixtools_install.sh
 echo "***Set file permissions"
 
-chown --reference=etc/hetrixtools_dl/hetrixtools_install_cp.sh etc/hetrixtools_dl/hetrixtools_install.sh
+chown --reference=/etc/hetrixtools_dl/hetrixtools_install_cp.sh /etc/hetrixtools_dl/hetrixtools_install.sh
 echo "***Set file owners"
 
-chmod u+r+x etc/hetrixtools_dl/hetrixtools_install.sh
+chmod u+r+x /etc/hetrixtools_dl/hetrixtools_install.sh
 echo "***Set as executable"
 
-etc/hetrixtools_dl/hetrixtools_install.sh $HETRIX_AGENT_PARAMS
+/etc/hetrixtools_dl/hetrixtools_install.sh $HETRIX_AGENT_PARAMS
 echo "***Installed agent"
 
 echo "***Starting cron"
